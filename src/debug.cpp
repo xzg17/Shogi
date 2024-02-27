3cffc9ac520b9662f12ad25f36668b316b03eda4
xzg17 <128256034+xzg17@users.noreply.github.com>
2024-02-27 10:43:01 +0900

    Update debug.cpp

--- src/debug.cpp
+++ src/debug.cpp
@@ -28,12 +28,12 @@ static PyObject *Board_push(Py_Class_Board *self, PyObject *args){
             return NULL;
         }
         int c_move[3]={
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 0)),
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 1)),
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 2))
+            (int)PyLong_AsLong(PyList_GetItem(move, 0)),
+            (int)PyLong_AsLong(PyList_GetItem(move, 1)),
+            (int)PyLong_AsLong(PyList_GetItem(move, 2))
         };
         self->board->push(move);
-    }else if(PyTuple_Check(move)){
+    }/*else if(PyTuple_Check(move)){
         if(PyTuple_Size(move)!=3){
             PyErr_SetString(PyExc_ValueError, "InitError_list2!");
             return NULL;
@@ -44,7 +44,7 @@ static PyObject *Board_push(Py_Class_Board *self, PyObject *args){
             (int)PyLong_AsLong(PyTuple_GetItem(pyboard, 2))
         };
         self->board->push(move);
-    };
+    };*/
     Py_INCREF(Py_None);
     return Py_None;
 };
