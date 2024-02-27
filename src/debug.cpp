c37f231483d553583195ab408cc85ce2fd5c0800
xzg17 <128256034+xzg17@users.noreply.github.com>
2024-02-27 10:40:08 +0900

    Update debug.cpp

--- src/debug.cpp
+++ src/debug.cpp
@@ -19,32 +19,8 @@ static PyObject *Board_ply(Py_Class_Board *self){
 static PyObject *Board_push(Py_Class_Board *self, PyObject *args){
     PyObject *move;
     if(!PyArg_ParseTuple(args, "O", &move)){
-        PyErr_SetString(PyExc_ValueError, "PushError1!");
+        PyErr_SetString(PyExc_ValueError, "Board.push() takes exactly one argument");
         return NULL;
-    };
-    if(PyList_Check(pyboard)){
-        if(PyList_Size(pyboard)!=3){
-            PyErr_SetString(PyExc_ValueError, "InitError_list2!");
-            return NULL;
-        }
-        int c_move[3]={
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 0)),
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 1)),
-            (int)PyLong_AsLong(PyList_GetItem(pyboard, 2))
-        };
-        self->board->push(move);
-    };
-    if(PyTuple_Check(pyboard)){
-        if(PyTuple_Size(pyboard)!=3){
-            PyErr_SetString(PyExc_ValueError, "InitError_list2!");
-            return NULL;
-        }
-        int c_move[3]={
-            (int)PyLong_AsLong(PyTuple_GetItem(pyboard, 0)),
-            (int)PyLong_AsLong(PyTuple_GetItem(pyboard, 1)),
-            (int)PyLong_AsLong(PyTuple_GetItem(pyboard, 2))
-        };
-        self->board->push(move);
     };
     Py_INCREF(Py_None);
     return Py_None;
